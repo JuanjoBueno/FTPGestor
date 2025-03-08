e1026328bf2752702aa4507e0595ae91e987d917
JuanjoBueno <jbaprogramacion@gmail.com>
2025-03-08 10:13:48 +0100

    monitor a falta de implementar hilos y probar

--- src/main/java/logica/FileMonitor.java
+++ src/main/java/logica/FileMonitor.java
@@ -1,4 +1,47 @@
 package logica;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
 public class FileMonitor {
+
+    private String localFolder;
+
+    public FileMonitor(String localFolder) {
+        this.localFolder = localFolder;
+    }
+
+    public void monitor() {
+
+        Map<String, Long> oldFilesMap = new HashMap<>();
+        System.out.println("Monitor de archivos iniciado en la carpeta " + localFolder);
+
+        while (true) {
+            File folder = new File(localFolder);
+            File[] files = folder.listFiles();
+            Map<String, Long> filesMap = new HashMap<>();
+
+            if (files != null) {
+                for (File file : files) {
+                    filesMap.put(file.getName(), file.lastModified());
+
+                    if (!oldFilesMap.containsKey(file.getName()) || oldFilesMap.get(file.getName()) != file.lastModified()) {
+                        //implementacion de la sincronizacion
+                    }
+                }
+            }
+            for (String fileName : oldFilesMap.keySet()) {
+                if (!filesMap.containsKey(fileName)) {
+                    //implementacion de la sincronizacion
+                }
+            }
+            oldFilesMap = filesMap;
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
