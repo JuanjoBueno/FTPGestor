c900cfe3d400c6760088ad27db86a39403f4e15e
JuanjoBueno <jbaprogramacion@gmail.com>
2025-03-08 09:39:45 +0100

    preparando el cifrado

--- src/main/java/logica/FTPManager.java
+++ src/main/java/logica/FTPManager.java
@@ -1,4 +1,24 @@
 package logica;
 
+import org.apache.commons.net.ftp.FTPClient;
+
 public class FTPManager {
+
+    private FTPClient clienteFTP;
+    private final String server;
+    private final int port;
+    private final String user;
+    private final String password;
+
+    public FTPManager(String server, int port, String user, String password) {
+        this.server = server;
+        this.port = port;
+        this.user = user;
+        this.password = password;
+        this.clienteFTP = new FTPClient();
+    }
+
+
+
+
 }
